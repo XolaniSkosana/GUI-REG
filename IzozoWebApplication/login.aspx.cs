0e5bdaf7047e4a6759a57adc31450ff69f908460
Nomvula98 <nomvulahportia@gmail.com>
2021-01-05 14:12:41 +0200

    login

--- IzozoWebApplication/login.aspx.cs
+++ IzozoWebApplication/login.aspx.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 public partial class login : System.Web.UI.Page
 {
+    OdbcConnection conn = new OdbcConnection(ConfigurationManager.ConnectionStrings["Dsn=izozoDS;Driver={MariaDB ODBC 3.1 Driver};tcpip=1;server=localhost;uid=root;database=izozodb"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -24,6 +30,23 @@ public partial class login : System.Web.UI.Page
 
     protected void btnLogin_Click(object sender, EventArgs e)
     {
+        conn.Open();
+        int numRows = int.Parse("SELECT COUNT(custUsername, custPassword) FROM tblCustomer WHERE custUsername = '" + username.Text + "' AND custPassword = '" + password.Text+"'");
+        
+
+        if (numRows == 1)
+        {
+            Response.Write("You have successfully logged in");
+        }
+        else
+        {
+            Response.Write("Inalid details");
+        }
+
+
+
+
+        conn.Close();
 
     }
 }
